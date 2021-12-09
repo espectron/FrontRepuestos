3c4bb37d0bb0b7db250924a2da735119091d7af2
Rafael Cañadulce <joserafaelc10@gmail.com>
2021-12-09 15:55:38 -0500

    Agg

--- src/app/components/agregar-editar-producto/agregar-editar-producto.component.ts
+++ src/app/components/agregar-editar-producto/agregar-editar-producto.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms'nor;
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Producto } from 'src/app/interfaces/Producto';
 
 @Component({
