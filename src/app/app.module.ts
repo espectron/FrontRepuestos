d1c9292e99e36650fc9979705e1838bad7c60801
Rafael <rafa_elk1996@hotmail.com>
2021-12-22 17:44:50 -0500

    All

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +23,8 @@ import { LoginComponent } from './components/login/login.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/producto.service.ts
+++ src/app/services/producto.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductoService {
 
-  constructor() { }
+  myAppUrl = "https://localhost:44369/";
+  myApiUrl = "api/Producto";
+
+  constructor(private http: HttpClient) { }
+
+  getListProductos(): Observable<any> {
+  return this.http.get(this.myAppUrl + this.myApiUrl);
+  }
 }
